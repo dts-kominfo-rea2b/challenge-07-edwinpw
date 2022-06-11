13a32675e7cd272a01e047b9d677dc0ee70f1eed
Edwin Prastyan <edwinprastyan88@gmail.com>
2022-06-11 22:56:20 +0700

    update

--- promise.js
+++ promise.js
@@ -1,7 +1,36 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
+const arrayTheater = [promiseTheaterIXX(),promiseTheaterVGC()];
+
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve,reject) => {
+    if(emosi == null || emosi == ""){
+      reject ("Emosi not found");
+    } else{
+      let totalEmosi = 0;
+      // const promiseTheather = [promiseTheaterIXX,promiseTheaterVGC];
+      promiseTheaterIXX()
+      .then((hasilEmosi) => {
+        hasilEmosi.forEach((theater) => {
+          if (theater.hasil === emosi) {
+            totalEmosi += 1;
+          }
+        });
+      })
+      .then(promiseTheaterVGC().then((hasilEmosi) => {
+          hasilEmosi.forEach((theater) => {
+            if (theater.hasil === emosi) {
+              totalEmosi += 1;
+            }
+          });
+          resolve(totalEmosi);
+        })
+      );
+    }
+  });
+
+};
 
 module.exports = {
   promiseOutput,
